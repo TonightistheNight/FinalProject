5b8dba60363400a0bcaaf5cf5393b9b74fd983b3
TonightistheNight <chrisrjs11@gmail.com>
2026-05-13 12:16:20 -0700

    Update Main.java
    So far what I've done

--- Main.java
+++ Main.java
@@ -1,30 +1,116 @@
 //Parent class
 class Character {
-    public void  attack (){
-        System.out.println("Character attacks!");
+    public void train(){
+        System.out.println("Character trains!");
     }
 }
 
 //Child 1
 class Warrior extends Character{
     @Override
-    public void attack(){
-        System.out.println("Warrior swings a sword!");
+    public void train(){
+        System.out.println("Warrior exercises!");
     }
 }
 
 //Child 2
 class Archer extends Character {
     @Override
-    public void attack(){
-        System.out.println("Archer shoots an arrow!");
+    public void train(){
+        System.out.println("Archer perfects aim!");
     }
 }
 
 //Child 3
 class Wizard extends Character {
     @Override
-    public void attack() {
-        System.out.println("Wizard casts a fireball!");
+    public void train() {
+        System.out.println("Wizard reads spellbook!");
+    }
+}
+
+//Child 4
+class Princess extends Character {
+    @Override
+        public void train(){
+            System.out.println("Princess studies 'Strategy of War' book!")
+    }
+}
+
+//Parent class
+class Character {
+    public void rest(){
+        System.out.println("Character rests!");
+    }
+}
+
+//Child 1
+class Warrior extends Character{
+    @Override
+    public void rest(){
+        System.out.println("Warrior takes a knee!");
+    }
+}
+
+//Child 2
+class Archer extends Character {
+    @Override
+    public void rest(){
+        System.out.println("Archer sleeps in the trees!");
+    }
+}
+
+//Child 3
+class Wizard extends Character {
+    @Override
+    public void rest() {
+        System.out.println("Wizard meditates!");
+    }
+}
+
+//Child 4
+class Princess extends Character {
+    @Override
+        public void rest(){
+            System.out.println("Princess sleeps in her castle!")
+    }
+}
+
+//Parent class
+class Character {
+    public void skill(){
+        System.out.println("Character performs a skill!");
+    }
+}
+
+//Child 1
+class Warrior extends Character{
+    @Override
+    public void skill(){
+        System.out.println("Warrior chops the head off an enemy!");
+    }
+}
+
+//Child 2
+class Archer extends Character {
+    @Override
+    public void skill(){
+        System.out.println("Archer shoots an arrow");
+    }
+}
+
+//Child 3
+class Wizard extends Character {
+    @Override
+    public void skill() {
+        System.out.println("Wizard casts a curse!");
+    }
+}
+
+//Child 4
+class Princess extends Character {
+    @Override
+        public void skill(){
+            System.out.println("Princess smacks her enemy with her golden crown!")
     }
 }
